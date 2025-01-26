dfb45adc2e35977a17bc3fddbabf0ca6b79bc0d0
Isara Chumsri <fasttocode.com>
2025-01-26 17:03:04 +0700

    05-create-home-card

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,71 @@
-import { HomeCarousel } from "@/components/shared/home/home-carousel";
-import data from "@/lib/data";
+import { HomeCard } from '@/components/shared/home/home-card'
+import { HomeCarousel } from '@/components/shared/home/home-carousel'
+import {
+  getProductsForCard,
+  getAllCategories,
+} from '@/lib/actions/product.actions'
+import data from '@/lib/data'
+import { toSlug } from '@/lib/utils'
 
 export default async function Page() {
-  return <HomeCarousel items={data.carousels} />;
+  const categories = (await getAllCategories()).slice(0, 4)
+  const newArrivals = await getProductsForCard({
+    tag: 'new-arrival',
+    limit: 4,
+  })
+  const featureds = await getProductsForCard({
+    tag: 'featured',
+    limit: 4,
+  })
+  const bestSellers = await getProductsForCard({
+    tag: 'best-seller',
+    limit: 4,
+  })
+  const cards = [
+    {
+      title: 'Categories to explore',
+      link: {
+        text: 'See More',
+        href: '/search',
+      },
+      items: categories.map((category) => ({
+        name: category,
+        image: `/images/${toSlug(category)}.jpg`,
+        href: `/search?category=${category}`,
+      })),
+    },
+    {
+      title: 'Explore New Arrivals',
+      items: newArrivals,
+      link: {
+        text: 'View All',
+        href: '/search?tag=new-arrival',
+      },
+    },
+    {
+      title: 'Discover Best Sellers',
+      items: bestSellers,
+      link: {
+        text: 'View All',
+        href: '/search?tag=new-arrival',
+      },
+    },
+    {
+      title: 'Featured Products',
+      items: featureds,
+      link: {
+        text: 'Shop Now',
+        href: '/search?tag=new-arrival',
+      },
+    },
+  ]
+
+  return (
+    <>
+      <HomeCarousel items={data.carousels} />
+      <div className='p-4 bg-background'>
+        <HomeCard cards={cards} />
+      </div>
+    </>
+  )
 }
