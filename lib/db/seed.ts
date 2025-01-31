b2c3b35ae2faa99a6bfcbb5db4687c5db30fcf9b
Isara Chumsri <fasttocode.com>
2025-01-31 22:14:46 +0700

    14-register-user

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -4,18 +4,25 @@ import { loadEnvConfig } from "@next/env";
 import data from "@/lib/data";
 import { connectToDatabase } from ".";
 import Product from "./models/product.model";
+import User from "./models/user.model";
 
 loadEnvConfig(cwd());
 
 const main = async () => {
   try {
-    const { products } = data;
+    const { products, users } = data;
     await connectToDatabase(process.env.MONGODB_URI);
+
+    await User.deleteMany()
+    const createdUser = await User.insertMany(users)
 
     await Product.deleteMany();
     const createdProducts = await Product.insertMany(products);
 
+
+
     console.log({
+      createdUser,
       createdProducts,
       message: "Database seeded successfully",
     });
--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,18 @@
 import { z } from "zod";
 
-import { CartSchema, OrderItemSchema, ProductInputSchema } from "@/lib/validator";
+import {
+  CartSchema,
+  OrderItemSchema,
+  ProductInputSchema,
+  UserInputSchema,
+  UserSignInSchema,
+  UserSignUpSchema
+} from "@/lib/validator";
 
 export type IProductInput = z.infer<typeof ProductInputSchema>;
 
 export type Data = {
+  users: IUserInput[];
   products: IProductInput[];
   headerMenus: {
     name: string;
@@ -21,3 +29,8 @@ export type Data = {
 
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
+
+// user
+export type IUserInput = z.infer<typeof UserInputSchema>
+export type IUserSignIn = z.infer<typeof UserSignInSchema>
+export type IUserSignUp = z.infer<typeof UserSignUpSchema>
